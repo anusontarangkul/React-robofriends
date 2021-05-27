33b6f2633a096c1cd418e6ff82387e83897090ef
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-05-26 21:20:55 -0700

    add fetch user JSON

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robot';
 
+import './App.css';
 
 
 
@@ -10,9 +10,15 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => { return response.json() })
+            .then(user => { this.setState({ robots: user }) })
     }
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
@@ -25,7 +31,7 @@ class App extends Component {
         })
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <CardList robots={filteredRobots} />
             </div>

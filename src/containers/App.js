e1dd5f20eec2548ebb8999178da63a03a2ab5c8f
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-05-30 14:48:38 -0700

    update redux provider

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     }
 
     componentDidMount() {
+
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => { return response.json() })
             .then(user => { this.setState({ robots: user }) })
